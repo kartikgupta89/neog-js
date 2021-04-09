d2e75a10b4f011472f104bb7920f994f69f39199
kartikgupta89 <kartikgupta89@gmail.com>
2021-04-09 15:43:51 +0530

    Speak the Queen's English final app

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#txt-output");
 
 var apiURL = "https://api.funtranslations.com/translate/shakespeare.json";
-function makeURL (text) {
+
+function makeURL(text) {
     return apiURL + "?text=" + text;
 }
 
 function clickEventHandler() {
     fetch(makeURL(txtInput.value))
         .then(response => response.json())
-        .then(json => {outputDiv.innerText = json.contents.translated});
+        .then(json => {
+            outputDiv.innerText = json.contents.translated
+        });
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
