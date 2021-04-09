0eebf5bd49b8c6162719b15c88a79f67b38c2cec
kartikgupta89 <kartikgupta89@gmail.com>
2021-04-09 15:09:18 +0530

    updated with minion api

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// var userName = prompt ('Who are you?');
-// alert (userName + ', this script is shit, eh?');
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#txt-output");
-// console.log(txtInput.value);
 
-var apiURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var apiURL = "https://api.funtranslations.com/translate/minion.json";
 function makeURL (text) {
     return apiURL + "?text=" + text;
 }
 
 function clickEventHandler() {
-    // console.log("clicked!");
-    // console.log(`input ${txtInput.value}`);
-
     fetch(makeURL(txtInput.value))
         .then(response => response.json())
         .then(json => {outputDiv.innerText = json.contents.translated});
